0acd367d305f058af4a93a73f302116db7a54013
youssefbayram <youssef.bayram@lau.edu>
2021-01-01 15:11:53 +0200

    Updated Button folder

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import { Story, Meta } from '@storybook/react/types-6-0'
-import { Button1, ButtonProps } from './Button'
+import { Button1} from './Button'
 import { Button } from '@material-ui/core'
 
 export default {
   title: 'Button',
   component: Button1,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as Meta
-
-//to show controls in storybook
-const Template: Story<ButtonProps> = (args) => <Button1 {...args} />
+}
 
 //2 states of button first deisgned by me second imported from materialui library
-export const First = Template.bind({})
-First.args = {
-  label: 'Button',
-  size:'large',
-}
+export const First = () => (
+  <Button1 label="Button">Button</Button1>
+)
 
 export const Second = () => (
   <Button variant="contained" color="primary">Button</Button>
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,41 +1,20 @@
 import React from 'react'
-import CSS from 'csstype'
-
-//Styling for button
-const h1Styles: CSS.Properties = {
-    padding:'0.46em 1.6em',
-    border:'0.1em solid #000000',
-    margin:'0 0.2em 0.2em 0',
-    borderRadius:'0.12em',
-    fontFamily:'monospace',
-    fontWeight:400,
-    fontSize: '15px',
-    color:'#FFFFFF',
-    textShadow: '0 0.04em 0.04em rgba(0,0,0,0.35)',
-    backgroundColor:'#ff414d',
-    textAlign:'center',
-    width:'150px'
-}
+import './Button.css'
 
 //props
-export interface ButtonProps {
-  backgroundColor?: string
-  size?: 'small' | 'medium' | 'large'
-  label: string
+interface ButtonProps {
+  label?: string
   onClick?: () => void
 }
 
 export const Button1: React.FC<ButtonProps> = ({
-  size='medium',
-  backgroundColor,
   label,
   ...props
 }) => {
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`].join(' ')}
-      style={h1Styles}
+      className="First"
       {...props}
     >
     {label}
